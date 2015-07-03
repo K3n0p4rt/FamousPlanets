dd3d5e1f3ae3ae54b5095db835866985e6a530f5
Kenrick Yap <K3n0p4rt@gmail.com>
2015-07-03 14:13:43 +0800

    Build orbit and light

--- src/index.js
+++ src/index.js
@@ -18,33 +18,37 @@ FamousEngine.init();
 // Initialize with a scene; then, add a 'node' to the scene root
 var scene = FamousEngine.createScene();
 
-/*var sizesystem = new SizeSystem();
-sizesystem.registerSizeAtPath(scene,250);
-*/
-var cameranode = scene.addChild();
-var camera = new Camera(cameranode);
-camera.setFrustum(200,220);
+//Light Constructor
+var Light = function (x, y, z, color) {
+	this.lightnode = scene.addChild();
+	this.lightmesh = new Mesh(this.lightnode);
+	this.light = new PointLight(this.lightnode);
+	this.light.setColor(new Color(color));
 
-//Light/Sun
-var lightnode = scene.addChild();
-var light = new PointLight(lightnode);
-light.setColor(new Color('white'));
+	this.lightnode.setSizeMode('absolute', 'absolute', 'absolute')
+    	.setPosition(x,y,z)
+    	.setAbsoluteSize(1, 1, 1)
+    	.setMountPoint(0.5, 0.5, 0.5)
+    	.setOrigin(0.5, 0.5, 0.5);
+}
 
+var light1 = new Light (200,400,400,'white')
+//var light2 = new Light (0,400,400,'lightgrey')
+//var light3 = new Light (0,200,0,'grey')
+//var light4 = new Light (0,600,0,'white')
 
-lightnode.setSizeMode('absolute', 'absolute', 'absolute')
-    .setPosition(1000,100,1000)
-    .setAbsoluteSize(500, 500, 500)
-    .setMountPoint(0.5, 0.5, 0.5)
-    .setOrigin(0.5, 0.5, 0.5);
-
-
-var Planet = function (x, y, z, size, period, textureDirectory) {
-    this.node = scene.addChild();
+//Planet Constructor
+var Planet = function (x, y, z, size, rperiod, operiod, textureDirectory) {
+    this.orbitor = scene.addChild();
+    this.node = this.orbitor.addChild();
+    this.rid = this.orbitor.addComponent(this);
     this.id = this.node.addComponent(this);
     this.mesh = new Mesh(this.node);
-    this.texture = Material.Texture(textureDirectory);
-    this.period = period;
+    this.texture = new Material.Texture(textureDirectory);
+    this.rperiod = rperiod;
     this.rotationpos = new Transitionable(0);
+    this.operiod = operiod;
+    this.orbitpos = new Transitionable(0);
 
     this.mesh.setGeometry('Sphere',{detail: 100,});
     this.mesh.setBaseColor(this.texture);
@@ -55,31 +59,51 @@ var Planet = function (x, y, z, size, period, textureDirectory) {
         .setMountPoint(0.5, 0.5, 0.5)
         .setOrigin(0.5, 0.5, 0.5);
 
-    this.node.requestUpdate(this.id);
-    
+    this.orbitor.setSizeMode('absolute','absolute','absolute')
+    	.setAbsoluteSize(1,1,1)
+    	.setPosition(0,0,0)
+    	.setMountPoint(0.5,0.5,0.5)
+    	.setOrigin(0.5,0.5,0.5);
+
+    this.node.requestUpdate(this.id);   
 };
 
 Planet.prototype.onUpdate = function onUpdate (time) {
-    if(Math.floor(time) % this.period < 20){
+    //rotator
+    if(Math.floor(time) % this.rperiod < 20){
         this.rotationpos.set(0);
-        this.rotationpos.set(6.28, {duration: this.period, curve: 'linear'});
+        this.rotationpos.set(6.28, {duration: this.rperiod, curve: 'linear'});
+    }
+    this.orbitor.setRotation(0,this.orbitpos.get(), 0);
+
+    //orbitor
+    if(Math.floor(time) % this.operiod < 20){
+        this.orbitpos.set(0);
+        this.orbitpos.set(6.28, {duration: this.operiod, curve: 'linear'});
     }
     this.node.setRotation(0,this.rotationpos.get(), 0);
     this.node.requestUpdateOnNextTick(this.id);
-
 }
 
-Planet.prototype.GetBig = function () {
-    this.node.setAbsoluteSize(200,200,200);
-}
+var time1 = 10
+var time2 = 10
 
-var earth = new Planet(400,400,0,50,4000, new Color('red'));
-var mars = new Planet(500,400,0,40,4000)
-var venus = new Planet(600,400,-100,200,-4000)
-var jupiter = new Planet(700,400,0,20,4000)
-var yomama = new Planet(800,400,0,10,4000)
-var kungpowchicken = new Planet(900,400,0,90,4000)
-var ckens = new Planet(1000,400,0,110,4000)
+var sunny = new Planet(0,400,0,300,600*time1,0,'/images/sun.jpg')
+var mercury = new Planet(200,400,0,40,1408*time1,88*time2,'/images/mercury.jpg')
+var venus = new Planet(300,400,0,48,2802*time1,225*time2,'/images/venus.jpg')
+var earth = new Planet(400,400,0,52,25*time1,364*time2,'/images/earth.jpg')
+var mars = new Planet(500,400,0,44,24*time1,687*time2,'/images/mars.jpg')
+var jupiter = new Planet(700,400,0,200,10*time1,4380*time2,'/images/jupiter.jpg')
+var saturn = new Planet(950,400,0,180,11*time1,10585*time2,'/images/saturn.jpg')
+var uranus = new Planet(1200,400,0,160,17*time1,30660*time2,'/images/uranus.jpg')
+var nepturn = new Planet(1450,400,0,160,16*time1,60225*time2,'/images/neptune.jpg')
+
+//Camera (WIP) (VERY EXPERIMENTAL)
+
+
+
+
+
 
 
 
